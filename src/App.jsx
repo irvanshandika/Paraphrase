4ec229f1a3cc47518074f67437ebb71f1f095756
irvanshandika <shandikamuhammadirvan@gmail>
2023-07-24 21:01:42 +0700

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 // src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPages from "./pages/MainPages";
+import BlankPages from "./pages/BlankPages";
 import Paraphrase from "./components/Paraphrase";
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Parafrase Web</h1>
-      <Paraphrase />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPages />} />
+        <Route path="/paraphrase" element={<Paraphrase />} />
+        <Route path="*" element={<BlankPages />} />
+      </Routes>
+    </Router>
   );
 }
 
