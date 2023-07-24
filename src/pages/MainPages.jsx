94f6b2615d8c8236741eda5927af948ad4521895
irvanshandika <shandikamuhammadirvan@gmail>
2023-07-24 21:20:47 +0700

    Website under development

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,11 +1,13 @@
 // src/MainPage.js
 import React from "react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 const MainPages = () => {
   return (
-    <div>
-      <h1>Welcome to Main Page</h1>
-      <p>Replace this content with your main page content.</p>
+    <div className="mt-20">
+      <Player autoplay loop src="https://lottie.host/0dde184f-13c3-4132-a019-86cca0ad5c33/HxOHzP0aX0.json" className="lg:w-[500px]">
+        <Controls visible={false} buttons={["play", "repeat", "frame", "debug"]} />
+      </Player>
     </div>
   );
 };
