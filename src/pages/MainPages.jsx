7fff96adfa29a5532f5852b94d6a5f0cba1bf573
irvanshandika <shandikamuhammadirvan@gmail>
2023-07-24 23:14:37 +0700

    Typo logo

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -8,7 +8,7 @@ const MainPages = () => {
     <div>
       <NavigasiBar />
       <div className="mt-8">
-        <Player autoplay loop src="https://lottie.host/0dde184f-13c3-4132-a019-86cca0ad5c33/HxOHzP0aX0.json" backgroundMode="transparent" className="lg:w-[500px]">
+        <Player autoplay loop src="https://lottie.host/0dde184f-13c3-4132-a019-86cca0ad5c33/HxOHzP0aX0.json" background="transparent" className="lg:w-[500px]">
           <Controls visible={false} buttons={["play", "repeat", "frame", "debug"]} />
         </Player>
       </div>
